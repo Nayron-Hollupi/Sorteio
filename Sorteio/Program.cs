9a0f5a463191c103fe8afb40b451cb82825f93a8
Nayron Hollupi <nhollupi@gmail.com>
2022-03-07 21:10:52 -0300

    Adicionar arquivos de projeto.

---
+++ Sorteio/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Sorteio
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random aleatorio = new Random();
+            int i, reserva,k, valor;
+            int[] numero = new int[20];
+
+            for (i = 0; i < numero.Length; i++)
+            {
+                valor = aleatorio.Next(1, 20);
+            
+                numero[i] = valor;
+
+                //Ordenacao 
+               
+                    for (k = i; k > 0; k--)
+                    {
+                        if (numero[k] < numero[k - 1])
+                        {
+                            reserva = numero[k - 1];
+                            numero[k - 1] = numero[k];
+                            numero[k] = reserva;
+                        }
+
+                    }
+                
+            }
+                      
+           
+         
+            for (i = 0; i < numero.Length; i++)
+            {
+                Console.WriteLine(" {0}" ,numero[i]);
+
+            }
+
+            }
+    }
+}
---
+++ Sorteio/Radom.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Sorteio
+{
+    internal class Radom
+    {
+        public static implicit operator Radom(Random v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
